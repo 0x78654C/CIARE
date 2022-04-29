5930aa3716275e4992ee85d43e64022c0973bf05
x_coding <l2xguard@live.com>
2022-04-30 00:51:22 +0300

    added following vars: regpath for registry management and roslyn code template

--- CIARE/Utils/GlobalVariables.cs
+++ CIARE/Utils/GlobalVariables.cs
@@ -5,5 +5,26 @@
         public static string paramData = string.Empty;
         public static string openedFilePath = string.Empty;
         public static bool savedFile = false;
+        public static readonly string registryPath = "SOFTWARE\\CIARE";
+        public static readonly string roslynTemplate = @"/*
+ * Simple C# code sample for run with Roslyn runtime code compiler and execution.
+ */
+
+using System;
+using System.Windows.Forms;
+// You can add more dependencies.
+
+namespace Test_Code
+{
+  public class Test
+  {	
+     public void Main(string arg)
+     {
+        //Do work here!
+     }
+  }
+}
+";
+
     }
 }
