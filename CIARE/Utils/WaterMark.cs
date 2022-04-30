02bf2d68c67a92cab1ea718cf1a16d8fac24058a
x_coding <l2xguard@live.com>
2022-05-01 01:35:42 +0300

    added watermark for textbox

---
+++ CIARE/Utils/WaterMark.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace CIARE.Utils
+{
+    public class WaterMark
+    {
+        //Grab the user32.dll function for textbox hint text
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, [MarshalAs(UnmanagedType.LPWStr)] string lParam);
+
+        /// <summary>
+        /// Set textbox watermark.
+        /// </summary>
+        /// <param name="textBox"></param>
+        /// <param name="message"></param>
+        public static void TextBoxWaterMark(TextBox textBox, string message)
+        {
+            SendMessage(textBox.Handle, 0x1501, 1, message);
+        }
+    }
+}
