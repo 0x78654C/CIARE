5e244e4588889b4178859c5f1e895d17ceec8b30
x_coding <l2xguard@live.com>
2022-05-05 00:12:25 +0300

    added WinForm for set binary name at compile time

---
+++ CIARE/BinaryName.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace CIARE
+{
+    partial class BinaryName
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BinaryName));
+            this.binaryNameTxt = new System.Windows.Forms.TextBox();
+            this.ConfirmButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // binaryNameTxt
+            // 
+            this.binaryNameTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.binaryNameTxt.Location = new System.Drawing.Point(43, 29);
+            this.binaryNameTxt.Name = "binaryNameTxt";
+            this.binaryNameTxt.Size = new System.Drawing.Size(196, 21);
+            this.binaryNameTxt.TabIndex = 0;
+            // 
+            // ConfirmButton
+            // 
+            this.ConfirmButton.Location = new System.Drawing.Point(23, 82);
+            this.ConfirmButton.Name = "ConfirmButton";
+            this.ConfirmButton.Size = new System.Drawing.Size(75, 23);
+            this.ConfirmButton.TabIndex = 1;
+            this.ConfirmButton.Text = "OK";
+            this.ConfirmButton.UseVisualStyleBackColor = true;
+            this.ConfirmButton.Click += new System.EventHandler(this.ConfirmButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(186, 82);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 2;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.binaryNameTxt);
+            this.groupBox1.Controls.Add(this.cancelButton);
+            this.groupBox1.Controls.Add(this.ConfirmButton);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(286, 126);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // BinaryName
+            // 
+            this.AcceptButton = this.ConfirmButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(308, 155);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BinaryName";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Set Binary Name";
+            this.Load += new System.EventHandler(this.BinaryName_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox binaryNameTxt;
+        private System.Windows.Forms.Button ConfirmButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ CIARE/BinaryName.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CIARE
+{
+    public partial class BinaryName : Form
+    {
+        /*
+         Form for handle binary name.
+         */
+        public BinaryName()
+        {
+            InitializeComponent();
+        }
+
+        private void BinaryName_Load(object sender, EventArgs e)
+        {
+            if (Utils.GlobalVariables.exeName)
+            {
+                this.Text = "Set EXE binary name";
+                Utils.WaterMark.TextBoxWaterMark(binaryNameTxt, "binary_file_name.exe");
+                return;
+            }
+            this.Text = "Set DLL binary name";
+            Utils.WaterMark.TextBoxWaterMark(binaryNameTxt, "binary_file_name.dll");
+        }
+
+        private void ConfirmButton_Click(object sender, EventArgs e)
+        {
+            if (binaryNameTxt.Text.Length > 0)
+            {
+                if (Utils.GlobalVariables.exeName && !binaryNameTxt.Text.ToLower().EndsWith(".exe"))
+                {
+                    MessageBox.Show("File name needs to end with '.exe' extension!", "CIARE", MessageBoxButtons.OK,
+ MessageBoxIcon.Warning);
+                    return;
+                }
+                if (Utils.GlobalVariables.exeName == false && !binaryNameTxt.Text.ToLower().EndsWith(".dll"))
+                {
+                    MessageBox.Show("File name needs to end with '.dll' extension!", "CIARE", MessageBoxButtons.OK,
+ MessageBoxIcon.Warning);
+                    return;
+                }
+                Utils.GlobalVariables.binaryName = binaryNameTxt.Text;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("You need to provide a name for the binary file!", "CIARE", MessageBoxButtons.OK,
+ MessageBoxIcon.Warning);
+            }
+            Utils.GlobalVariables.exeName = false;
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
