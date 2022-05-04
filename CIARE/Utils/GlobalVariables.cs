a0a907c475a867799ca6f19da309cfa138f44cfa
x_coding <l2xguard@live.com>
2022-05-05 00:11:43 +0300

    new variables added for binary name and binary type check

--- CIARE/Utils/GlobalVariables.cs
+++ CIARE/Utils/GlobalVariables.cs
@@ -5,20 +5,21 @@
         public static string paramData = string.Empty;
         public static string openedFilePath = string.Empty;
         public static bool savedFile = false;
+        public static bool exeName = false;
+        public static string binaryName = string.Empty; 
         public static readonly string registryPath = "SOFTWARE\\CIARE";
         public static readonly string roslynTemplate = @"/*
  * Simple C# code sample for run with Roslyn runtime code compiler and execution.
  */
 
 using System;
-using System.Windows.Forms;
 // You can add more dependencies.
 
 namespace Test_Code
 {
   class Test
   {	
-     static void Main(string[] arg)
+     static void Main(string[] args)
      {
         //Do work here!
      }
