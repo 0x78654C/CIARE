bffb84b6f4dac21ba4c26fd7412acba2ca799cfb
x_coding <l2xguard@live.com>
2022-05-05 23:01:47 +0300

    Fixed issue #1 for not clearing binary name on form close

--- CIARE/BinaryName.cs
+++ CIARE/BinaryName.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using CIARE.Utils;
 
 namespace CIARE
 {
@@ -8,6 +9,8 @@ namespace CIARE
         /*
          Form for handle binary name.
          */
+
+        private bool _checkConfirmationAction = false;
         public BinaryName()
         {
             InitializeComponent();
@@ -15,33 +18,35 @@ namespace CIARE
 
         private void BinaryName_Load(object sender, EventArgs e)
         {
-            if (Utils.GlobalVariables.exeName)
+            GlobalVariables.checkFormOpen = true;
+            if (GlobalVariables.exeName)
             {
                 this.Text = "Set EXE binary name";
                 Utils.WaterMark.TextBoxWaterMark(binaryNameTxt, "binary_file_name.exe");
                 return;
             }
             this.Text = "Set DLL binary name";
-            Utils.WaterMark.TextBoxWaterMark(binaryNameTxt, "binary_file_name.dll");
+            WaterMark.TextBoxWaterMark(binaryNameTxt, "binary_file_name.dll");
         }
 
         private void ConfirmButton_Click(object sender, EventArgs e)
         {
             if (binaryNameTxt.Text.Length > 0)
             {
-                if (Utils.GlobalVariables.exeName && !binaryNameTxt.Text.ToLower().EndsWith(".exe"))
+                if (GlobalVariables.exeName && !binaryNameTxt.Text.ToLower().EndsWith(".exe"))
                 {
                     MessageBox.Show("File name needs to end with '.exe' extension!", "CIARE", MessageBoxButtons.OK,
  MessageBoxIcon.Warning);
                     return;
                 }
-                if (Utils.GlobalVariables.exeName == false && !binaryNameTxt.Text.ToLower().EndsWith(".dll"))
+                if (GlobalVariables.exeName == false && !binaryNameTxt.Text.ToLower().EndsWith(".dll"))
                 {
                     MessageBox.Show("File name needs to end with '.dll' extension!", "CIARE", MessageBoxButtons.OK,
  MessageBoxIcon.Warning);
                     return;
                 }
-                Utils.GlobalVariables.binaryName = binaryNameTxt.Text;
+                GlobalVariables.binaryName = binaryNameTxt.Text;
+                _checkConfirmationAction = true;
                 this.Close();
             }
             else
@@ -49,12 +54,30 @@ namespace CIARE
                 MessageBox.Show("You need to provide a name for the binary file!", "CIARE", MessageBoxButtons.OK,
  MessageBoxIcon.Warning);
             }
-            Utils.GlobalVariables.exeName = false;
+            GlobalVariables.exeName = false;
         }
 
+        /// <summary>
+        /// Clear binary variables on cance button event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void cancelButton_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        /// <summary>
+        /// Clear binary variables on form close.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BinaryName_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            GlobalVariables.checkFormOpen = false;
+            GlobalVariables.exeName = false;
+            if (!_checkConfirmationAction)
+                GlobalVariables.binaryName = string.Empty;
         }
     }
 }
