182f9186e0af0ed8cfa2addee87625b2a9a4029d
x_coding <l2xguard@live.com>
2022-05-05 23:02:31 +0300

    chaned design to be more compact

--- CIARE/BinaryName.Designer.cs
+++ CIARE/BinaryName.Designer.cs
@@ -40,14 +40,15 @@ namespace CIARE
             // binaryNameTxt
             // 
             this.binaryNameTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.binaryNameTxt.Location = new System.Drawing.Point(43, 29);
+            this.binaryNameTxt.Location = new System.Drawing.Point(12, 19);
             this.binaryNameTxt.Name = "binaryNameTxt";
             this.binaryNameTxt.Size = new System.Drawing.Size(196, 21);
             this.binaryNameTxt.TabIndex = 0;
+            this.binaryNameTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // ConfirmButton
             // 
-            this.ConfirmButton.Location = new System.Drawing.Point(23, 82);
+            this.ConfirmButton.Location = new System.Drawing.Point(12, 63);
             this.ConfirmButton.Name = "ConfirmButton";
             this.ConfirmButton.Size = new System.Drawing.Size(75, 23);
             this.ConfirmButton.TabIndex = 1;
@@ -58,7 +59,7 @@ namespace CIARE
             // cancelButton
             // 
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(186, 82);
+            this.cancelButton.Location = new System.Drawing.Point(133, 63);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 2;
@@ -73,7 +74,7 @@ namespace CIARE
             this.groupBox1.Controls.Add(this.ConfirmButton);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(286, 126);
+            this.groupBox1.Size = new System.Drawing.Size(222, 96);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             // 
@@ -84,7 +85,7 @@ namespace CIARE
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.CancelButton = this.cancelButton;
-            this.ClientSize = new System.Drawing.Size(308, 155);
+            this.ClientSize = new System.Drawing.Size(245, 126);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -93,6 +94,7 @@ namespace CIARE
             this.Name = "BinaryName";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Set Binary Name";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.BinaryName_FormClosed);
             this.Load += new System.EventHandler(this.BinaryName_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
