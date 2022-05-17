901ff03a1f99159cfad550dd2fb93c43864a5da8
x_coding <l2xguard@live.com>
2022-05-17 23:33:50 +0300

    added Show/Hide split container second Panel class

---
+++ CIARE/Utils/SplitContainerHideShow.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Forms;
+namespace CIARE.Utils
+{
+    /*
+     Show/Hide split container second Panel.
+     */
+    public class SplitContainerHideShow
+    {
+        /// <summary>
+        /// Hide split container second panel.
+        /// </summary>
+        /// <param name="splitContainer"></param>
+        public static void HideSplitContainer(SplitContainer splitContainer)
+        {
+            splitContainer.Panel2Collapsed = true;
+            splitContainer.Panel2.Hide();
+        }
+
+        /// <summary>
+        /// Show split container second panel.
+        /// </summary>
+        /// <param name="splitContainer"></param>
+        public static void ShowSplitContainer(SplitContainer splitContainer)
+        {
+            splitContainer.Panel2Collapsed = false;
+            splitContainer.Panel2.Show();
+        }
+    }
+}
