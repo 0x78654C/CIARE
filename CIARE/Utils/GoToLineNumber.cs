81a3bff179ae73affed81ab150a395eaec4f0fda
x_coding <l2xguard@live.com>
2022-05-19 00:23:46 +0300

    added class for handle Go To Line for ICSharpCode.TextEditor

---
+++ CIARE/Utils/GoToLineNumber.cs
@@ -0,0 +1,19 @@
+﻿using ICSharpCode.TextEditor;
+
+namespace CIARE.Utils
+{
+    public class GoToLineNumber
+    {
+        /// <summary>
+        /// Got to line number.
+        /// </summary>
+        /// <param name="textEditorControl"></param>
+        /// <param name="lineNumber"></param>
+        public static void GoToLine(TextEditorControl textEditorControl , int lineNumber)
+        {
+            TextArea textArea = textEditorControl.ActiveTextAreaControl.TextArea;
+            textArea.Caret.Line = lineNumber - 1;
+            textArea.Caret.UpdateCaretPosition();
+        }
+    }
+}
