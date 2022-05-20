de8db63ae230863842a6c7bd1ffdeb5ffd997ccd
x_coding <l2xguard@live.com>
2022-05-21 00:50:01 +0300

    added form for set Command Line Arguments used at runtime code execution

---
+++ CIARE/CmdLineArgs.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace CIARE
+{
+    partial class CmdLineArgs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CmdLineArgs));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmdLineArgTxtBox = new System.Windows.Forms.TextBox();
+            this.confirmBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cancelBtn);
+            this.groupBox1.Controls.Add(this.confirmBtn);
+            this.groupBox1.Controls.Add(this.cmdLineArgTxtBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(318, 163);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Set Command Line Arguments:";
+            // 
+            // cmdLineArgTxtBox
+            // 
+            this.cmdLineArgTxtBox.Location = new System.Drawing.Point(6, 19);
+            this.cmdLineArgTxtBox.Multiline = true;
+            this.cmdLineArgTxtBox.Name = "cmdLineArgTxtBox";
+            this.cmdLineArgTxtBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.cmdLineArgTxtBox.Size = new System.Drawing.Size(305, 78);
+            this.cmdLineArgTxtBox.TabIndex = 0;
+            // 
+            // confirmBtn
+            // 
+            this.confirmBtn.Location = new System.Drawing.Point(6, 116);
+            this.confirmBtn.Name = "confirmBtn";
+            this.confirmBtn.Size = new System.Drawing.Size(75, 23);
+            this.confirmBtn.TabIndex = 1;
+            this.confirmBtn.Text = "OK";
+            this.confirmBtn.UseVisualStyleBackColor = true;
+            this.confirmBtn.Click += new System.EventHandler(this.confirmBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelBtn.Location = new System.Drawing.Point(236, 116);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.TabIndex = 2;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // CmdLineArgs
+            // 
+            this.AcceptButton = this.confirmBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.CancelButton = this.cancelBtn;
+            this.ClientSize = new System.Drawing.Size(344, 195);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CmdLineArgs";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Command Line Arguments";
+            this.Load += new System.EventHandler(this.CmdLineArgs_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.Button confirmBtn;
+        private System.Windows.Forms.TextBox cmdLineArgTxtBox;
+    }
+}
---
+++ CIARE/CmdLineArgs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CIARE.Utils;
+using System.Windows.Forms;
+
+namespace CIARE
+{
+    /*
+     Form for set the command line arguments.
+     */
+    public partial class CmdLineArgs : Form
+    {
+        public CmdLineArgs()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdLineArgs_Load(object sender, EventArgs e)
+        {
+            cmdLineArgTxtBox.Text= GlobalVariables.commandLineArguments;
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void confirmBtn_Click(object sender, EventArgs e)
+        {
+            GlobalVariables.commandLineArguments = cmdLineArgTxtBox.Text;
+            this.Close();
+        }
+    }
+}
