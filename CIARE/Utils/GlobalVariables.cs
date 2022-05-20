638ccbf6a4ece5b94a43bff4186942ba93eec4ba
x_coding <l2xguard@live.com>
2022-05-21 00:46:35 +0300

    added new variables for command line argumnts, output display flag, and store binary name on confirm button

--- CIARE/Utils/GlobalVariables.cs
+++ CIARE/Utils/GlobalVariables.cs
@@ -4,10 +4,13 @@
     {
         public static string paramData = string.Empty;
         public static string openedFilePath = string.Empty;
+        public static string commandLineArguments = string.Empty;
         public static bool savedFile = false;
         public static bool exeName = false;
         public static bool checkFormOpen = false;
+        public static bool outPutDisplay = false;
         public static string binaryName = string.Empty; 
+        public static string binaryNameStore = string.Empty; 
         public static readonly string registryPath = "SOFTWARE\\CIARE";
         public static readonly string roslynTemplate = @"/*
  * Simple C# code sample for run with Roslyn runtime code compiler and execution.
