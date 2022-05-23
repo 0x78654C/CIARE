f9edd66b8c572cea7da074d61dcc19fa17026f0d
x_coding <l2xguard@live.com>
2022-05-23 23:47:19 +0300

    added class for change Menu items selection color.

---
+++ CIARE/GUI/ColorTableSet.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace CIARE.GUI
+{
+    /// <summary>
+    /// Class for change Menu items selection color.
+    /// </summary>
+    public class ColorTableSet : ToolStripProfessionalRenderer
+    {
+        public ColorTableSet() : base(new MyColors()) { }
+        private class MyColors : ProfessionalColorTable
+        {
+            public override Color MenuItemSelected
+            {
+                get { return Color.FromArgb(45, 45, 48); }
+            }
+
+            public override Color MenuItemSelectedGradientBegin
+            {
+                get { return Color.FromArgb(45, 45, 48); }
+            }
+            public override Color MenuItemSelectedGradientEnd
+            {
+                get { return Color.FromArgb(45, 45, 48); }
+            }
+            public override Color MenuItemPressedGradientBegin => Color.FromArgb(45, 45, 48);
+            public override Color MenuItemPressedGradientEnd => Color.FromArgb(45, 45, 48);
+
+        }
+    }
+}
