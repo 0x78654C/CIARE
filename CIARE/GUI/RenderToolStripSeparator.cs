615f539730465d88041292e4186c353d8f0b2a4b
x_coding <l2xguard@live.com>
2022-05-23 20:48:32 +0300

    added renderer for ToolStripSeparator backgound/foreground color used on toolstrip menu in main form

---
+++ CIARE/GUI/RenderToolStripSeparator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CIARE.GUI
+{
+    /// <summary>
+    /// Render ToolStripSeparator backgound/foreground color used on toolstrip menu in main form..
+    /// </summary>
+    public class RenderToolStripSeparator
+    {
+        /// <summary>
+        /// Render ToolStripSeparator backgound/foreground color for light
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static  void RenderToolStripSeparator_PaintDark(object sender, PaintEventArgs e)
+        {
+            ToolStripSeparator toolStripSeparator = (ToolStripSeparator)sender;
+            int width = toolStripSeparator.Width;
+            int height = toolStripSeparator.Height;
+            Color foreColor = Color.FromArgb(192, 215, 207);
+            Color backColor = Color.FromArgb(51, 51, 51);
+            e.Graphics.FillRectangle(new SolidBrush(backColor), 0, 0, width, height);
+            e.Graphics.DrawLine(new Pen(foreColor), 4, height / 2, width - 4, height / 2);
+        }
+
+        /// <summary>
+        /// Render ToolStripSeparator backgound/foreground color.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static void RenderToolStripSeparator_PaintLight(object sender, PaintEventArgs e)
+        {
+            ToolStripSeparator toolStripSeparator = (ToolStripSeparator)sender;
+            int width = toolStripSeparator.Width;
+            int height = toolStripSeparator.Height;
+            Color foreColor = SystemColors.ControlDark;
+            Color backColor = SystemColors.Window;
+            e.Graphics.FillRectangle(new SolidBrush(backColor), 0, 0, width, height);
+            e.Graphics.DrawLine(new Pen(foreColor), 4, height / 2, width - 4, height / 2);
+        }
+    }
+}
