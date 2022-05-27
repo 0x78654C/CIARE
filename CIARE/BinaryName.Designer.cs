3a9ecc41ba7e9d9abf81b20c70e6c29cd9e7503c
x_coding <l2xguard@live.com>
2022-05-28 00:29:57 +0300

    set text size to 10

--- CIARE/BinaryName.Designer.cs
+++ CIARE/BinaryName.Designer.cs
@@ -39,10 +39,10 @@ namespace CIARE
             // 
             // binaryNameTxt
             // 
-            this.binaryNameTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.binaryNameTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.binaryNameTxt.Location = new System.Drawing.Point(12, 19);
             this.binaryNameTxt.Name = "binaryNameTxt";
-            this.binaryNameTxt.Size = new System.Drawing.Size(196, 21);
+            this.binaryNameTxt.Size = new System.Drawing.Size(196, 22);
             this.binaryNameTxt.TabIndex = 0;
             this.binaryNameTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
--- CIARE/CmdLineArgs.Designer.cs
+++ CIARE/CmdLineArgs.Designer.cs
@@ -75,7 +75,7 @@ namespace CIARE
             // 
             // cmdLineArgTxtBox
             // 
-            this.cmdLineArgTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdLineArgTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmdLineArgTxtBox.Location = new System.Drawing.Point(6, 19);
             this.cmdLineArgTxtBox.Multiline = true;
             this.cmdLineArgTxtBox.Name = "cmdLineArgTxtBox";
