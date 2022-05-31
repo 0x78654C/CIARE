2cd19043a87b571779c420d6c6a533416dce8804
x_coding <l2xguard@live.com>
2022-06-01 00:02:59 +0300

    refactor: created class for output window manage (show/hide)

---
+++ CIARE/GUI/OutputWindowManage.cs
@@ -0,0 +1,34 @@
+﻿using CIARE.Utils;
+using System.Windows.Forms;
+
+namespace CIARE.GUI
+{
+    /*
+     Class for manage output window (show/hide).
+     */
+    public class OutputWindowManage
+    {
+        /// <summary>
+        /// Pop up the output pane on compile or code run.
+        /// </summary>
+        public static void ShowOutputOnCompileRun(bool runner, SplitContainer splitContainer, RichTextBox outLogRtb)
+        {
+            if (runner)
+            {
+                SplitContainerHideShow.ShowSplitContainer(splitContainer);
+                Form1.Instance._visibleSplitContainer = false;
+                GlobalVariables.outPutDisplay = false;
+                outLogRtb.Focus();
+                return;
+            }
+
+            if (GlobalVariables.outPutDisplay)
+            {
+                SplitContainerHideShow.ShowSplitContainer(splitContainer);
+                Form1.Instance._visibleSplitContainer = false;
+                GlobalVariables.outPutDisplay = false;
+                outLogRtb.Focus();
+            }
+        }
+    }
+}
