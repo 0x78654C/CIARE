8cd2fea37cf36988802606fa5bb7cba865dc450a
x_coding <l2xguard@live.com>
2022-06-01 01:12:11 +0300

    refactor: moved SetOutputWindowState() methotd from main form.

--- CIARE/GUI/OutputWindowManage.cs
+++ CIARE/GUI/OutputWindowManage.cs
@@ -11,6 +11,9 @@ namespace CIARE.GUI
         /// <summary>
         /// Pop up the output pane on compile or code run.
         /// </summary>
+        /// <param name="runner"></param>
+        /// <param name="splitContainer"></param>
+        /// <param name="outLogRtb"></param>
         public static void ShowOutputOnCompileRun(bool runner, SplitContainer splitContainer, RichTextBox outLogRtb)
         {
             if (runner)
@@ -28,6 +31,30 @@ namespace CIARE.GUI
                 Form1.Instance._visibleSplitContainer = false;
                 GlobalVariables.outPutDisplay = false;
                 outLogRtb.Focus();
+            }
+        }
+
+        /// <summary>
+        /// Set output window state.
+        /// </summary>
+        /// <param name="outputRtb"></param>
+        /// <param name="splitContainer"></param>
+        public static void SetOutputWindowState(RichTextBox outputRtb,SplitContainer splitContainer)
+        {
+            if (Form1.Instance._visibleSplitContainer)
+            {
+                SplitContainerHideShow.ShowSplitContainer(splitContainer);
+                Form1.Instance._visibleSplitContainer = false;
+                Form1.Instance._visibleSplitContainerAutoHide = true;
+                outputRtb.Focus();
+                RegistryManagement.RegKey_WriteSubkey(GlobalVariables.registryPath, "OutWState", "False");
+            }
+            else
+            {
+                SplitContainerHideShow.HideSplitContainer(splitContainer);
+                Form1.Instance._visibleSplitContainer = true;
+                Form1.Instance._visibleSplitContainerAutoHide = false;
+                RegistryManagement.RegKey_WriteSubkey(GlobalVariables.registryPath, "OutWState", "True");
             }
         }
     }
