4f488c09b1faccddceeae6f00c20ccb630193d62
x_coding <l2xguard@live.com>
2022-09-12 23:51:03 +0300

    added build options that include configuration/platform selection  and warning enable/disable

--- CIARE/Options.cs
+++ CIARE/Options.cs
@@ -8,6 +8,9 @@ namespace CIARE
 {
     public partial class Options : Form
     {
+        /// <summary>
+        /// TODO: add compile param /p:Platform=
+        /// </summary>
         public Options()
         {
             InitializeComponent();
@@ -28,19 +31,20 @@ namespace CIARE
         {
             InitializeEditor.ReadEditorHighlight(GlobalVariables.registryPath, Form1.Instance.textEditorControl1, highlightCMB);
             if (GlobalVariables.darkColor)
-                DarkMode.OptionsDarkMode(this, closeBtn, highlightLbl, highlightCMB, codeCompletionCkb, lineNumberCkb, codeFoldingCkb);
+                DarkMode.OptionsDarkMode(this, closeBtn, highlightLbl, highlightCMB, codeCompletionCkb, lineNumberCkb, codeFoldingCkb, displayGroup, buildGroup);
             codeCompletionCkb.Checked = GlobalVariables.OCodeCompletion;
             lineNumberCkb.Checked = GlobalVariables.OLineNumber;
             codeFoldingCkb.Checked = GlobalVariables.OFoldingCode;
+            warningsCkb.Checked = GlobalVariables.OWarnings;
         }
 
         private void highlightCMB_SelectedIndexChanged(object sender, EventArgs e)
         {
             Form1.Instance.SetHighLighter(highlightCMB.Text);
             if (GlobalVariables.darkColor)
-                DarkMode.OptionsDarkMode(this, closeBtn, highlightLbl, highlightCMB, codeCompletionCkb, lineNumberCkb, codeFoldingCkb);
+                DarkMode.OptionsDarkMode(this, closeBtn, highlightLbl, highlightCMB, codeCompletionCkb, lineNumberCkb, codeFoldingCkb, displayGroup, buildGroup);
             else
-                LightMode.OptionsLightMode(this, closeBtn, highlightLbl, highlightCMB, codeCompletionCkb, lineNumberCkb, codeFoldingCkb);
+                LightMode.OptionsLightMode(this, closeBtn, highlightLbl, highlightCMB, codeCompletionCkb, lineNumberCkb, codeFoldingCkb, displayGroup, buildGroup);
         }
 
         private void codeCompletionCkb_CheckedChanged(object sender, EventArgs e)
@@ -56,6 +60,17 @@ namespace CIARE
         private void codeFoldingCkb_CheckedChanged(object sender, EventArgs e)
         {
             FoldingCode.SetFoldingCodeStatus(codeFoldingCkb, GlobalVariables.foldingCodeKey);
+        }
+
+
+        /// <summary>
+        /// Enable build warnings.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void warningsCkb_CheckedChanged(object sender, EventArgs e)
+        {
+            Warnings.SetWarnings(warningsCkb, GlobalVariables.warnings);
         }
     }
 }
