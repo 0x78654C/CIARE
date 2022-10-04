95f5c9d195d2feaa71e0326bac16c43cd78ec7a7
x_coding <l2xguard@live.com>
2022-10-04 23:11:34 +0300

    added run process for single instance in visible window state

--- CIARE/Utils/ProcessRun.cs
+++ CIARE/Utils/ProcessRun.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Linq;
 
 namespace CIARE.Utils
 {
@@ -22,7 +23,7 @@ namespace CIARE.Utils
         }
 
         /// <summary>
-        /// Run procces.
+        /// Run procces with console output redirect.
         /// </summary>
         /// <returns></returns>
         public string Run()
@@ -54,6 +55,21 @@ namespace CIARE.Utils
             process.BeginOutputReadLine();
             process.WaitForExit();
             return outData;
+        }
+
+        /// <summary>
+        /// Run process in visible mode without wait for exit.
+        /// </summary>
+        public void RunVisible()
+        {
+            if (string.IsNullOrEmpty(ProcessToRun) || string.IsNullOrEmpty(Arguments))
+                return;
+            ProcessStartInfo startInfo = new ProcessStartInfo(ProcessToRun);
+            startInfo.UseShellExecute = false;
+            startInfo.Arguments = Arguments;
+            Process process = new Process();
+            process.StartInfo = startInfo;
+            process.Start();
         }
     }
 }
