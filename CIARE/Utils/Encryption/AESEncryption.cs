9560a93879132f1816751cec0c09af5f01f22dc1
x_coding <l2xguard@live.com>
2022-11-03 22:20:33 +0200

    add: AES-256 encryption for live code shared

---
+++ CIARE/Utils/Encryption/AESEncryption.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+
+namespace CIARE.Utils.Encryption
+{
+    public class AESEncryption
+    {
+        private static readonly Encoding encoding = Encoding.UTF8;
+
+
+        /// <summary>
+        /// AES Encryption
+        /// </summary>
+        /// <param name="plainText">String input for encryption.</param>
+        /// <param name="password">Master Password</param>>
+        /// <returns>string</returns>
+        public static string Encrypt(string plainText, string password)
+        {
+            try
+            {
+                RijndaelManaged aes = new RijndaelManaged
+                {
+                    KeySize = 256,
+                    BlockSize = 128,
+                    Padding = PaddingMode.PKCS7,
+                    Mode = CipherMode.CBC,
+                    Key = CreateKey(password)
+                };
+                aes.GenerateIV();
+                ICryptoTransform AESEncrypt = aes.CreateEncryptor(aes.Key, aes.IV);
+                byte[] buffer = encoding.GetBytes(plainText);
+                string encryptedText = Convert.ToBase64String(AESEncrypt.TransformFinalBlock(buffer, 0, buffer.Length));
+                String mac = "";
+                mac = BitConverter.ToString(HmacSHA256(Convert.ToBase64String(aes.IV) + encryptedText, password)).Replace("-", "").ToLower();
+                var keyValues = new Dictionary<string, object>
+                {
+                    { "iv", Convert.ToBase64String(aes.IV) },
+                    { "value", encryptedText },
+                    { "mac", mac },
+                };
+                return Convert.ToBase64String(encoding.GetBytes(JsonSerializer.Serialize(keyValues)));
+            }
+            catch (Exception e)
+            {
+                return "Error encrypting: " + e.ToString();
+            }
+        }
+
+        /// <summary>
+        /// AES Decryption 
+        /// </summary>
+        /// <param name="plainText">String input for decryption</param>
+        /// <param name="password">Master Password</param>
+        /// <returns>string</returns>
+        public static string Decrypt(string plainText, string password)
+        {
+            try
+            {
+                RijndaelManaged aes = new RijndaelManaged
+                {
+                    KeySize = 256,
+                    BlockSize = 128,
+                    Padding = PaddingMode.PKCS7,
+                    Mode = CipherMode.CBC,
+                    Key = CreateKey(password)
+                };
+                byte[] base64Decoded = Convert.FromBase64String(plainText);
+                string base64DecodedStr = encoding.GetString(base64Decoded);
+                var payload = JsonSerializer.Deserialize<Dictionary<string, string>>(base64DecodedStr);
+                aes.IV = Convert.FromBase64String(payload["iv"]);
+                ICryptoTransform AESDecrypt = aes.CreateDecryptor(aes.Key, aes.IV);
+                byte[] buffer = Convert.FromBase64String(payload["value"]);
+                return encoding.GetString(AESDecrypt.TransformFinalBlock(buffer, 0, buffer.Length));
+            }
+            catch (Exception e)
+            {
+                return "Error decrypting: " + e.ToString();
+            }
+        }
+
+        private static byte[] CreateKey(string password, int keyBytes = 32)
+        {
+            const int Iterations = 300;
+            var keyGenerator = new Rfc2898DeriveBytes(password.Substring(2, 10), Salt, Iterations);
+            return keyGenerator.GetBytes(keyBytes);
+        }
+
+        private static readonly byte[] Salt = new byte[] { 10, 20, 30, 40, 50, 60, 70, 80 };
+
+        /// <summary>
+        /// Hash computation with SHA256
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        private static byte[] HmacSHA256(String data, String key)
+        {
+            using (HMACSHA256 hmac = new HMACSHA256(encoding.GetBytes(key)))
+            {
+                return hmac.ComputeHash(encoding.GetBytes(data));
+            }
+        }
+    }
+}
