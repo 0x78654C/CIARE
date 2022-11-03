df3c4c44a27d58370d66e479844f0fe4ebaa84a6
x_coding <l2xguard@live.com>
2022-11-03 22:23:16 +0200

    add: display current session id, password

---
+++ CIARE/LiveShareHost.cs
@@ -0,0 +1,54 @@
+﻿using CIARE.GUI;
+using CIARE.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIARE
+{
+    [SupportedOSPlatform("windows")]
+    public partial class LiveShareHost : Form
+    {
+        public LiveShareHost()
+        {
+            InitializeComponent();
+        }
+
+        private void LiveShareHost_Load(object sender, EventArgs e)
+        {
+            // Set dark mode if enabled.
+            if (GlobalVariables.darkColor)
+                DarkMode.LiveShareDarkMode(this, liveShareStartGrp, sessionIdLbl, sessionTxt, passwordLbl, passwordTxt, startLiveBtn,
+                    remoteGrp, remoteSessionLbl, remoteSessioniDtxt, remotePassLbl, remotePasswordTxt, connectHostBtn);
+
+            // Generate secure encryption password.
+            GeneratePassword(passwordTxt);
+
+            // Display current session id.
+            // Generated only on first app run.
+            SetSessionId(sessionTxt);
+
+        }
+        private void startLiveBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GeneratePassword(TextBox password)
+        {
+            password.Text = Utils.Encryption.KeyGenerator.GeneratePassword(15, true, true, false);
+        }
+
+        private void SetSessionId(TextBox sessionId)
+        {
+            sessionId.Text = GlobalVariables.sessionId;
+        }
+    }
+}
