1fbd3dbe8504058d506ff500d5a6f94b0dbdae20
x_coding <l2xguard@live.com>
2022-11-07 23:28:23 +0200

    change: moved ApiConnection method from main form to apiconnecitonevents.

--- CIARE/LiveShareManage/ApiConnectionEvents.cs
+++ CIARE/LiveShareManage/ApiConnectionEvents.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CIARE.Utils;
@@ -10,6 +11,59 @@ namespace CIARE.LiveShareManage
 {
     public class ApiConnectionEvents
     {
+        /// <summary>
+        /// Build connection events to API.
+        /// </summary>
+        /// <param name="updateCode"></param>
+        /// <param name="writer"></param>
+        /// <param name="connected"></param>
+        public static void ApiConnection(HubConnection hubConnection, PictureBox liveStatusPb,TextEditorControl textEditorControl, bool connected, string apiUrl)
+        {
+            if (string.IsNullOrEmpty(apiUrl))
+                return;
+            var timeOut = TimeSpan.FromSeconds(60);
+
+
+            hubConnection.ServerTimeout = timeOut;
+
+            // Reconnecting event.
+            hubConnection.Reconnecting += (sender) =>
+            {
+                connected = false;
+                if (GlobalVariables.typeConnection)
+                    GlobalVariables.apiConnected = false;
+                else
+                    GlobalVariables.apiRemoteConnected = false;
+                liveStatusPb.Image = Properties.Resources.orange_dot;
+                return Task.CompletedTask;
+            };
+
+            // Reconnected event.
+            hubConnection.Reconnected += (sender) =>
+            {
+                connected = true;
+                if (GlobalVariables.typeConnection)
+                    GlobalVariables.apiConnected = true;
+                else
+                    GlobalVariables.apiRemoteConnected = true;
+                hubConnection.InvokeAsync("GetSendCode", GlobalVariables.sessionId, string.Empty);
+                liveStatusPb.Image = Properties.Resources.red_dot;
+                textEditorControl.Text = textEditorControl.Text;
+                return Task.CompletedTask;
+            };
+
+            // Closed event.
+            hubConnection.Closed += (sender) =>
+            {
+                connected = false;
+                if (GlobalVariables.typeConnection)
+                    GlobalVariables.apiConnected = false;
+                else
+                    GlobalVariables.apiRemoteConnected = false;
+                liveStatusPb.Image = null;
+                return Task.CompletedTask;
+            };
+        }
 
         /// <summary>
         /// Connect to remote session.
@@ -26,6 +80,8 @@ namespace CIARE.LiveShareManage
                 connectBtn.Text = "Remote Connect";
                 MessageBox.Show("Connected Stoped", "CIARE", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 liveShareBtn.Enabled = true;
+                if(GlobalVariables.darkColor)
+                    liveShareBtn.BackColor = Color.FromArgb(30, 30, 30);
             }
             else
             {
@@ -57,6 +113,8 @@ namespace CIARE.LiveShareManage
                     connectBtn.Text = "Stop Connection";
                     MessageBox.Show("Connected to remote session!", "CIARE", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     liveShareBtn.Enabled = false;
+                    if (GlobalVariables.darkColor)
+                        liveShareBtn.BackColor =Color.Gray;
                 }
                 catch { }
             }
@@ -111,6 +169,8 @@ namespace CIARE.LiveShareManage
                     await hubConnection.StopAsync();
 
                 connectBtn.Enabled = true;
+                if (GlobalVariables.darkColor)
+                    connectBtn.BackColor = Color.FromArgb(30, 30, 30);
                 GlobalVariables.livePassword = string.Empty;
                 GlobalVariables.apiConnected = false;
                 GlobalVariables.connected = false;
@@ -138,6 +198,9 @@ namespace CIARE.LiveShareManage
                     GlobalVariables.apiConnected = true;
                     GlobalVariables.connected = true;
                     connectBtn.Enabled = false;
+                    if (GlobalVariables.darkColor)
+                        connectBtn.BackColor = Color.Gray;
+                 
                     MessageBox.Show("Live Share started!", "CIARE", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 catch { }
