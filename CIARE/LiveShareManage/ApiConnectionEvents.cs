a23fbc4510ebf10d2599b4740bc6404f0570bc07
x_coding <l2xguard@live.com>
2022-11-11 00:16:35 +0200

    removed reconnect and reconnectnig hub event and set disconnection event to picturebox image change and added connection error on start share/remote

--- CIARE/LiveShareManage/ApiConnectionEvents.cs
+++ CIARE/LiveShareManage/ApiConnectionEvents.cs
@@ -17,49 +17,15 @@ namespace CIARE.LiveShareManage
         /// <param name="updateCode"></param>
         /// <param name="writer"></param>
         /// <param name="connected"></param>
-        public static void ApiConnection(HubConnection hubConnection, PictureBox liveStatusPb,TextEditorControl textEditorControl, bool connected, string apiUrl)
+        public static void ApiConnection(HubConnection hubConnection, PictureBox liveStatusPb, TextEditorControl textEditorControl, bool connected, string apiUrl)
         {
             if (string.IsNullOrEmpty(apiUrl))
                 return;
-            var timeOut = TimeSpan.FromSeconds(60);
-
-
-            hubConnection.ServerTimeout = timeOut;
-
-            // Reconnecting event.
-            hubConnection.Reconnecting += (sender) =>
-            {
-                connected = false;
-                if (GlobalVariables.typeConnection)
-                    GlobalVariables.apiConnected = false;
-                else
-                    GlobalVariables.apiRemoteConnected = false;
-                liveStatusPb.Image = Properties.Resources.orange_dot;
-                return Task.CompletedTask;
-            };
-
-            // Reconnected event.
-            hubConnection.Reconnected += (sender) =>
-            {
-                connected = true;
-                if (GlobalVariables.typeConnection)
-                    GlobalVariables.apiConnected = true;
-                else
-                    GlobalVariables.apiRemoteConnected = true;
-                hubConnection.InvokeAsync("GetSendCode", GlobalVariables.sessionId, string.Empty);
-                liveStatusPb.Image = Properties.Resources.red_dot;
-                textEditorControl.Text = textEditorControl.Text;
-                return Task.CompletedTask;
-            };
 
             // Closed event.
             hubConnection.Closed += (sender) =>
             {
-                connected = false;
-                if (GlobalVariables.typeConnection)
-                    GlobalVariables.apiConnected = false;
-                else
-                    GlobalVariables.apiRemoteConnected = false;
+                GlobalVariables.liveDisconnected = true;
                 liveStatusPb.Image = null;
                 return Task.CompletedTask;
             };
@@ -73,14 +39,16 @@ namespace CIARE.LiveShareManage
         {
             if (GlobalVariables.apiRemoteConnected)
             {
+                GlobalVariables.apiRemoteConnected = false;
+
                 if (hubConnection != null)
                     await hubConnection.StopAsync();
+
                 GlobalVariables.connected = false;
-                GlobalVariables.apiRemoteConnected = false;
                 connectBtn.Text = "Remote Connect";
-                MessageBox.Show("Connected Stoped", "CIARE", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Connected Stoped", "CIARE - Live Share", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 liveShareBtn.Enabled = true;
-                if(GlobalVariables.darkColor)
+                if (GlobalVariables.darkColor)
                     liveShareBtn.BackColor = Color.FromArgb(30, 30, 30);
             }
             else
@@ -88,35 +56,41 @@ namespace CIARE.LiveShareManage
 
                 if (string.IsNullOrEmpty(password))
                 {
-                    MessageBox.Show("No password provied!", "CIARE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    MessageBox.Show("No password provied!", "CIARE  - Live Share", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     return;
                 }
 
                 if (string.IsNullOrEmpty(sessionId))
                 {
-                    MessageBox.Show("You must enter the remote session ID to connect!", "CIARE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    MessageBox.Show("You must enter the remote session ID to connect!", "CIARE - Live Share", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     return;
                 }
 
-                hubConnection.On<string, int>("GetSend", (code, lineNumber) =>
+                hubConnection.On<string, int, string>("GetSend", (code, lineNumber, remoteConnectionId) =>
                 {
+                    GlobalVariables.remoteConnectionId = remoteConnectionId;
                     SetLiveCode(textEditorControl, code, password, lineNumber);
                 });
 
                 try
                 {
                     await hubConnection.StartAsync();
-                    await hubConnection.InvokeAsync("GetSendCode", sessionId, string.Empty, 0);
+                    await hubConnection.InvokeAsync("GetSendCode", sessionId, "connected", 0);
                     pictureBox.Image = Properties.Resources.red_dot;
                     GlobalVariables.apiRemoteConnected = true;
                     GlobalVariables.connected = true;
+                    GlobalVariables.liveDisconnected = false;
                     connectBtn.Text = "Stop Connection";
-                    MessageBox.Show("Connected to remote session!", "CIARE", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("Connected to remote session!", "CIARE - Live Share", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     liveShareBtn.Enabled = false;
                     if (GlobalVariables.darkColor)
-                        liveShareBtn.BackColor =Color.Gray;
+                        liveShareBtn.BackColor = Color.Gray;
                 }
-                catch { }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "CIARE - Live Share", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    connectBtn.Text = "Remote Connect";
+                }
             }
         }
 
@@ -129,7 +103,7 @@ namespace CIARE.LiveShareManage
         {
             try
             {
-                if (!GlobalVariables.codeWriter)
+                if (!GlobalVariables.codeWriter && hubConnection.ConnectionId != GlobalVariables.remoteConnectionId)
                 {
                     int lineNumber = 0;
                     lineNumber = GoToLineNumber.GetLineNumber(textEditorControl);
@@ -165,6 +139,7 @@ namespace CIARE.LiveShareManage
             if (GlobalVariables.apiConnected)
             {
                 startShareBtn.Text = "Start Live Share";
+                GlobalVariables.apiConnected = false;
                 if (hubConnection != null)
                     await hubConnection.StopAsync();
 
@@ -172,38 +147,43 @@ namespace CIARE.LiveShareManage
                 if (GlobalVariables.darkColor)
                     connectBtn.BackColor = Color.FromArgb(30, 30, 30);
                 GlobalVariables.livePassword = string.Empty;
-                GlobalVariables.apiConnected = false;
                 GlobalVariables.connected = false;
-                MessageBox.Show("Live Share stopped!", "CIARE", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Live Share stopped!", "CIARE - Live Share", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else
             {
                 startShareBtn.Text = "Stop Live Share";
                 if (string.IsNullOrEmpty(password))
                 {
-                    MessageBox.Show("No password provied!", "CIARE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    MessageBox.Show("No password provied!", "CIARE - Live Share", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     return;
                 }
 
-                hubConnection.On<string, int>("GetSend", (code, lineNumber) =>
+                hubConnection.On<string, int, string>("GetSend", (code, lineNumber, remoteConnectionId) =>
                 {
+                    GlobalVariables.remoteConnectionId = remoteConnectionId;
                     SetLiveCode(textEditorControl, code, password, lineNumber);
                 });
 
                 try
                 {
                     await hubConnection.StartAsync();
-                    await hubConnection.InvokeAsync("GetSendCode", sessionId, string.Empty, 0);
+                    await hubConnection.InvokeAsync("GetSendCode", sessionId, "connected", 0);
                     pictureBox.Image = Properties.Resources.red_dot;
                     GlobalVariables.apiConnected = true;
                     GlobalVariables.connected = true;
+                    GlobalVariables.liveDisconnected = false;
                     connectBtn.Enabled = false;
                     if (GlobalVariables.darkColor)
                         connectBtn.BackColor = Color.Gray;
-                 
-                    MessageBox.Show("Live Share started!", "CIARE", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    MessageBox.Show("Live Share started!", "CIARE - Live Share", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
-                catch { }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "CIARE - Live share", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    startShareBtn.Text = "Start Live Share";
+                }
             }
         }
 
@@ -216,18 +196,59 @@ namespace CIARE.LiveShareManage
         {
             try
             {
-                if (!string.IsNullOrEmpty(code))
+                if (!string.IsNullOrEmpty(code) && code != "reconnected")
                 {
                     var decrypt = AESEncryption.Decrypt(code, password);
                     if (!string.IsNullOrEmpty(decrypt))
                     {
                         textEditorControl.Text = decrypt;
                         GoToLineNumber.GoToLine(textEditorControl, lineNumber);
+                        textEditorControl.Refresh();
                     }
                 }
                 GlobalVariables.codeWriter = true;
             }
             catch { }
+        }
+        /// <summary>
+        /// Set timespan to 60 secconds for hub to reconnect(every 10 sec a reconnection).
+        /// </summary>
+        public static TimeSpan[] DefaultBackoffTimes = new TimeSpan[]
+        {
+            TimeSpan.Zero,
+            TimeSpan.FromSeconds(10),
+            TimeSpan.FromSeconds(20),
+            TimeSpan.FromSeconds(30),
+        };
+
+
+        /// <summary>
+        /// Handle live hub disconnection.
+        /// </summary>
+        /// <param name="textEditorControl"></param>
+        public static async void ManageHubDisconnection(HubConnection hubConnection)
+        {
+            GlobalVariables.apiConnected = false;
+            GlobalVariables.apiRemoteConnected = false;
+            GlobalVariables.connected = false;
+            if (hubConnection != null)
+                await hubConnection.StopAsync();
+
+            DialogResult dr = DialogResult.No;
+            dr = MessageBox.Show("The live share connection is down. Do you want to reconnect?", "CIARE - Live Share", MessageBoxButtons.YesNo,
+MessageBoxIcon.Warning);
+
+            var fakeButton = new Button();
+
+            if (dr == DialogResult.Yes)
+            {
+                if (GlobalVariables.typeConnection)
+                    await StartShare(hubConnection, GlobalVariables.livePassword, GlobalVariables.sessionId,
+               fakeButton, fakeButton, Form1.Instance.textEditorControl1, Form1.Instance.liveStatusPb);
+                else
+                    await Connect(hubConnection, fakeButton, fakeButton,
+GlobalVariables.livePassword, GlobalVariables.sessionId, Form1.Instance.textEditorControl1, Form1.Instance.liveStatusPb);
+            }
         }
     }
 }
