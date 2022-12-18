02a260bc0b51bbb6dec61f266ac05e759bb2b38c
x_coding <l2xguard@live.com>
2022-12-19 01:57:50 +0200

    fixed index error and set corespond message to cases

--- CIARE/Utils/OpenAISettings/AiManage.cs
+++ CIARE/Utils/OpenAISettings/AiManage.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Windows.Controls;
 using System.Windows.Forms;
 
 namespace CIARE.Utils.OpenAISettings
@@ -22,6 +21,10 @@ namespace CIARE.Utils.OpenAISettings
             Qestion = question;
         }
 
+        /// <summary>
+        /// OPenAI API call
+        /// </summary>
+        /// <returns></returns>
         private async Task<string> AskOpenAI()
         {
             if (string.IsNullOrEmpty(ApiKey))
@@ -41,6 +44,11 @@ namespace CIARE.Utils.OpenAISettings
             return resu.Choices.First().Text;
         }
 
+        /// <summary>
+        /// Get data from OpenAI
+        /// </summary>
+        /// <param name="textEditorControl"></param>
+        /// <param name="apiAi"></param>
         public static async void GetDataAI(TextEditorControl textEditorControl, string apiAi)
         {
             if (string.IsNullOrEmpty(apiAi))
@@ -48,8 +56,24 @@ namespace CIARE.Utils.OpenAISettings
                 MessageBox.Show("OpenAI API key was not found!", "CIARE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
-            var question = textEditorControl.Text.MiddleString("/*[", "]*/");
-            if (string.IsNullOrWhiteSpace(question))
+            string question = string.Empty;
+            if (textEditorControl.Text.Contains("/*[") && textEditorControl.Text.Contains("]*/"))
+            {
+                try
+                {
+                    question = textEditorControl.Text.MiddleString("/*[", "]*/");
+                    if (string.IsNullOrWhiteSpace(question))
+                    {
+                        MessageBox.Show("No question provided!", "CIARE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                }
+                catch
+                {
+                    // Do nothing.
+                }
+            }
+            else
             {
                 MessageBox.Show("Wrong question format!", "CIARE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
