39c2e1563cfedd7bffab854a9490ca85b731320e
x_coding <l2xguard@live.com>
2023-02-03 22:20:56 +0200

    wip: added method to replace custom string with the namespace from as using directive from library

--- CIARE/Reference/CustomRef.cs
+++ CIARE/Reference/CustomRef.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.Loader;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CIARE.Utils;
+using ICSharpCode.TextEditor;
+using System.Diagnostics.Eventing.Reader;
+using Microsoft.Win32;
+using System.Runtime.Versioning;
 
 namespace CIARE.Reference
 {
+    [SupportedOSPlatform("Windows")]
     public class CustomRef
     {
         private static bool IsManaged(string libName)
@@ -24,13 +32,66 @@ namespace CIARE.Reference
             return false;
         }
 
-        public static void LoadCustomAssembly(string libName, RichTextBox logOutput)
+        /// <summary>
+        /// Loads custom managed library and sets namespace as directive in editor.
+        /// </summary>
+        /// <param name="textEditorControl"></param>
+        /// <param name="logOutput"></param>
+        public static void SetCustomRefDirective(TextEditorControl textEditorControl, RichTextBox logOutput)
         {
-            if (!IsManaged(libName))
+            try
+            {
+                if (string.IsNullOrEmpty(textEditorControl.Text))
+                    return;
+                var textData = textEditorControl.Text;
+                var reader = new StringReader(textData);
+                string line = "";
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.StartsWith("using [\"") && line.EndsWith("\"]"))
+                    {
+                        var libPath = line.Replace("using [\"", string.Empty).Trim();
+                        libPath = libPath.Replace("\"]", string.Empty).Trim();
+
+                        if (!File.Exists(libPath))
+                            logOutput.Text += $"Reference library not found: {libPath}\n";
+
+                        if (IsManaged(libPath))
+                        {
+                            var asmName = Assembly.LoadFile(libPath).GetTypes().Select(t => t.Namespace).Last();
+                            var refList = GlobalVariables.customRefAsm;
+                            if (!refList.Contains(libPath))
+                            {
+                                refList.Add(libPath);
+                                MainForm.pcRegistry.LoadCustomAssembly(libPath);
+                            }
+                            textData = textData.Replace(line, $"using {asmName}; // Custom ref: {libPath}");
+                        }
+                    }
+                }
+                textEditorControl.Text = textData;
+            }
+            catch (Exception ex)// Exception is for tests at this point
+            {
+                logOutput.Text = $"Error: {ex.Message}";
+            }
+        }
+
+
+
+        public static void LoadCustomAssembly(string libPath, RichTextBox logOutput)
+        {
+            if (!File.Exists(libPath))
+                logOutput.Text = $"Reference library not found: {libPath}";
+            if (!IsManaged(libPath))
                 return;
             try
             {
-              //  Assembly.LoadFrom(libName);
+                var asmName = Assembly.LoadFile(libPath).GetTypes().Select(t => t.Namespace).Last();
+
+                var refList = GlobalVariables.customRefAsm;
+                if (!refList.Contains(libPath))
+                    refList.Add(libPath);
             }
             catch (Exception ex)// Exception is for tests at this point
             {
