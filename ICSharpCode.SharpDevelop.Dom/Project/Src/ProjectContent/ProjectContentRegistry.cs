066fe4a15f24922036889a9f6a02e433475087d9
x_coding <l2xguard@live.com>
2023-02-07 22:24:39 +0200

    small cleanup

--- ICSharpCode.SharpDevelop.Dom/Project/Src/ProjectContent/ProjectContentRegistry.cs
+++ ICSharpCode.SharpDevelop.Dom/Project/Src/ProjectContent/ProjectContentRegistry.cs
@@ -10,7 +10,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Threading;
 using System.Xml;
 
 namespace ICSharpCode.SharpDevelop.Dom
