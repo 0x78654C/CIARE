c65a1056cf6c78c81ebf04b65388ee87bf1f8e10
x_coding <l2xguard@live.com>
2023-02-08 22:15:42 +0200

    added cancel buton and load refence libraries to list method with delete and copy to clipboard function

--- CIARE/Model/RefManager.Designer.cs
+++ CIARE/Model/RefManager.Designer.cs
@@ -28,12 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RefManager));
             this.refLisgGroupBox = new System.Windows.Forms.GroupBox();
             this.refListView = new System.Windows.Forms.ListView();
+            this.nameSpace = new System.Windows.Forms.ColumnHeader();
             this.filePath = new System.Windows.Forms.ColumnHeader();
-            this.addRefFileBtn = new System.Windows.Forms.Button();
+            this.AddRefFileBtn = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.deleteStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyNamespaceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.refLisgGroupBox.SuspendLayout();
+            this.deleteStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // refLisgGroupBox
@@ -49,6 +56,7 @@
             // refListView
             // 
             this.refListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nameSpace,
             this.filePath});
             this.refListView.Location = new System.Drawing.Point(6, 22);
             this.refListView.Name = "refListView";
@@ -56,29 +64,69 @@
             this.refListView.TabIndex = 0;
             this.refListView.UseCompatibleStateImageBehavior = false;
             this.refListView.View = System.Windows.Forms.View.Details;
+            this.refListView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.refListView_MouseClick);
+            // 
+            // nameSpace
+            // 
+            this.nameSpace.Text = "Namespace";
+            this.nameSpace.Width = 132;
             // 
             // filePath
             // 
             this.filePath.Text = "File Path";
-            this.filePath.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.filePath.Width = 595;
+            this.filePath.Width = 580;
             // 
-            // addRefFileBtn
+            // AddRefFileBtn
             // 
-            this.addRefFileBtn.Location = new System.Drawing.Point(12, 343);
-            this.addRefFileBtn.Name = "addRefFileBtn";
-            this.addRefFileBtn.Size = new System.Drawing.Size(106, 28);
-            this.addRefFileBtn.TabIndex = 1;
-            this.addRefFileBtn.Text = "Add Reference";
-            this.addRefFileBtn.UseVisualStyleBackColor = true;
-            this.addRefFileBtn.Click += new System.EventHandler(this.addRefFileBtn_Click);
+            this.AddRefFileBtn.Location = new System.Drawing.Point(12, 343);
+            this.AddRefFileBtn.Name = "AddRefFileBtn";
+            this.AddRefFileBtn.Size = new System.Drawing.Size(106, 28);
+            this.AddRefFileBtn.TabIndex = 1;
+            this.AddRefFileBtn.Text = "Add Reference";
+            this.AddRefFileBtn.UseVisualStyleBackColor = true;
+            this.AddRefFileBtn.Click += new System.EventHandler(this.AddRefFileBtn_Click);
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.Location = new System.Drawing.Point(638, 343);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(106, 28);
+            this.CancelBtn.TabIndex = 2;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
+            // 
+            // deleteStrip
+            // 
+            this.deleteStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteToolStripMenuItem,
+            this.copyNamespaceToolStripMenuItem});
+            this.deleteStrip.Name = "deleteStrip";
+            this.deleteStrip.Size = new System.Drawing.Size(181, 70);
+            this.deleteStrip.Text = "Delete";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deleteToolStripMenuItem.Text = "Delete reference";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // copyNamespaceToolStripMenuItem
+            // 
+            this.copyNamespaceToolStripMenuItem.Name = "copyNamespaceToolStripMenuItem";
+            this.copyNamespaceToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.copyNamespaceToolStripMenuItem.Text = "Copy Namespace";
+            this.copyNamespaceToolStripMenuItem.Click += new System.EventHandler(this.copyNamespaceToolStripMenuItem_Click);
             // 
             // RefManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.CancelBtn;
             this.ClientSize = new System.Drawing.Size(756, 397);
-            this.Controls.Add(this.addRefFileBtn);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.AddRefFileBtn);
             this.Controls.Add(this.refLisgGroupBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -88,6 +136,7 @@
             this.Text = "Reference Manager";
             this.Load += new System.EventHandler(this.RefManager_Load);
             this.refLisgGroupBox.ResumeLayout(false);
+            this.deleteStrip.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -98,6 +147,11 @@
         private System.Windows.Forms.ListView refListView;
         private System.Windows.Forms.ColumnHeader @namespace;
         private System.Windows.Forms.ColumnHeader filePath;
-        private System.Windows.Forms.Button addRefFileBtn;
+        private System.Windows.Forms.Button AddRefFileBtn;
+        private System.Windows.Forms.ColumnHeader nameSpace;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.ContextMenuStrip deleteStrip;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyNamespaceToolStripMenuItem;
     }
 }
--- CIARE/Model/RefManager.cs
+++ CIARE/Model/RefManager.cs
@@ -1,17 +1,14 @@
 ﻿using CIARE.GUI;
+using CIARE.Reference;
 using CIARE.Utils;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Versioning;
 using System.Windows.Forms;
 
 namespace CIARE.Model
 {
+    [SupportedOSPlatform("Windows")]
     public partial class RefManager : Form
     {
         public RefManager()
@@ -21,12 +18,99 @@ namespace CIARE.Model
 
         private void RefManager_Load(object sender, EventArgs e)
         {
+            // Set dark mode if enabled.
             FrmColorMod.ToogleColorMode(this, GlobalVariables.darkColor);
+
+            // Populate listview with ref.
+            PopulateList(GlobalVariables.customRefAsm, ref refListView);
         }
 
-        private void addRefFileBtn_Click(object sender, EventArgs e)
+        private void AddRefFileBtn_Click(object sender, EventArgs e)
         {
+            // Add lib's to list dialog.
+            FileManage.AddReferenceDialog();
 
+            // Repopulate listview with ref. after loading list.
+            PopulateList(GlobalVariables.customRefAsm, ref refListView);
+
+            // Load assemblies from list.
+            CustomRef.SetCustomRefDirective(GlobalVariables.customRefAsm, MainForm.Instance.outputRBT);
         }
+
+        /// <summary>
+        /// Cancel current form.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CancelBtn_Click(object sender, EventArgs e) => this.Close();
+
+        /// <summary>
+        /// Populate the listview with reference lib path and namespace.
+        /// </summary>
+        /// <param name="libPath"></param>
+        /// <param name="refList"></param>
+        private void PopulateList(List<string> libPath, ref ListView refList)
+        {
+            foreach (var lib in libPath)
+            {
+                string assemblyNamespace = CustomRef.GetAssemblyNamespace(lib);
+                ListViewItem item = new ListViewItem(new[] { assemblyNamespace, lib });
+                var foudItem = refList.FindItemWithText(assemblyNamespace);
+                if (foudItem == null)
+                    refList.Items.Add(item);
+            }
+        }
+
+        /// <summary>
+        /// Open delete menu for selected item on right click.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void refListView_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                var focusedItem = refListView.FocusedItem;
+                if (focusedItem != null && focusedItem.Bounds.Contains(e.Location))
+                    deleteStrip.Show(Cursor.Position);
+            }
+        }
+
+        /// <summary>
+        /// Delete reference library from lists.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DeleteRefLibrary(refListView);
+        }
+
+        /// <summary>
+        /// Remove reference library from list.
+        /// </summary>
+        /// <param name="refList"></param>
+        private void DeleteRefLibrary(ListView refList)
+        {
+            var selecItem = refList.SelectedItems[0].SubItems[1].Text;
+            GlobalVariables.customRefAsm.Remove(selecItem);
+            refList.SelectedItems[0].Remove();
+        }
+        
+        /// <summary>
+        /// Set namespace to clipborad.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void copyNamespaceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CopyNamespace(refListView);
+        }
+        
+        /// <summary>
+        /// Set text to clipborad from reference list.
+        /// </summary>
+        /// <param name="refList"></param>
+        private void CopyNamespace(ListView refList) => Clipboard.SetText(refList.SelectedItems[0].Text);
     }
 }
