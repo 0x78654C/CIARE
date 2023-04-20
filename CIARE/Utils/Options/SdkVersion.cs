e791fd490eb712dbd34a990333a3f5890a7927a4
x_Coding <xcoding.dev@gmail.com>
2023-04-20 22:51:38 +0300

    added class for check sdk installed version

---
+++ CIARE/Utils/Options/SdkVersion.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CIARE.Utils.Options
+{
+    public class SdkVersion
+    {
+        /// <summary>
+        ///  Get sdk version.
+        /// </summary>
+        /// <returns></returns>
+        private static List<string> Versions()
+        {
+            List<string> outList = new List<string>();
+            var processRun = new ProcessRun("dotnet", "--list-sdks", Application.UserAppDataPath);
+            var sdkResult = processRun.Run();
+            using (var reader = new StringReader(sdkResult))
+            {
+                var line = string.Empty;
+                while (null != (line = reader.ReadLine()))
+                {
+                    if (!string.IsNullOrWhiteSpace(line))
+                    {
+                        int len = line.Length - 1;
+                        outList.Add(line[..^len]);
+                    }
+                }
+            }
+            return outList;
+        }
+
+        /// <summary>
+        /// Check if current sdk exist.
+        /// </summary>
+        /// <param name="comboBox"></param>
+        /// <param name="sdkVersionStart"></param>
+        public static bool CheckSdk(string selectedVersion) => Versions().Contains(selectedVersion);
+    }
+}
