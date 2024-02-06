b71e0d22112af88016ab542716a4a977bede2dd3
xeL <xcoding.dev@gmail.com>
2024-02-06 17:58:43 +0200

    created class for getting edtior controler from selected tab

---
+++ CIARE/GUI/SelectedEditor.cs
@@ -0,0 +1,22 @@
+﻿using ICSharpCode.TextEditor;
+using System.Windows.Forms;
+using System.Runtime.Versioning;
+
+namespace CIARE.GUI
+{
+    [SupportedOSPlatform("Windows")]
+    public class SelectedEditor
+    {
+        /// <summary>
+        /// Get editor controler from selected tab.
+        /// </summary>
+        /// <returns></returns>
+        public static TextEditorControl GetSelectedEditor()
+        {
+            int selectedTab = MainForm.Instance.EditorTabControl.SelectedIndex;
+            Control ctrl = MainForm.Instance.EditorTabControl.Controls[selectedTab].Controls[0];
+            var textEditor = ctrl as TextEditorControl;
+            return textEditor;
+        }
+    }
+}
