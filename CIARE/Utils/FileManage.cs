b76de400798429e33967569b75c7f7502b81c2c6
xeL <xcoding.dev@gmail.com>
2024-02-24 18:29:24 +0200

    fix * dispaly on file

--- CIARE/Utils/FileManage.cs
+++ CIARE/Utils/FileManage.cs
@@ -258,7 +258,7 @@ MessageBoxIcon.Warning);
                     if (tab.Text.StartsWith("*") || tab.Text.Contains("New Page"))
                     {
                         if (!string.IsNullOrEmpty(textEditorControl.Text))
-                            dr = MessageBox.Show($"There is unsaved data in {MainForm.Instance.EditorTabControl.SelectedTab.Text.Trim()}. Do you want to save it?", "CIARE", MessageBoxButtons.YesNoCancel,
+                            dr = MessageBox.Show($"There is unsaved data in {MainForm.Instance.EditorTabControl.SelectedTab.Text.Trim().Replace("*","")}. Do you want to save it?", "CIARE", MessageBoxButtons.YesNoCancel,
             MessageBoxIcon.Warning);
                         MainForm.Instance.EditorTabControl.SelectTab(tab);
                         DialogResultAction(dr, textEditorControl);
@@ -269,7 +269,7 @@ MessageBoxIcon.Warning);
                     if ((tab.Text.StartsWith("*") || tab.Text.Contains("New Page")) && isSelectedTab)
                     {
                         if (!string.IsNullOrEmpty(textEditorControl.Text))
-                            dr = MessageBox.Show($"There is unsaved data in {MainForm.Instance.EditorTabControl.SelectedTab.Text.Trim()}. Do you want to save it?", "CIARE", MessageBoxButtons.YesNoCancel,
+                            dr = MessageBox.Show($"There is unsaved data in {MainForm.Instance.EditorTabControl.SelectedTab.Text.Trim().Replace("*", "")}. Do you want to save it?", "CIARE", MessageBoxButtons.YesNoCancel,
             MessageBoxIcon.Warning);
                         DialogResultAction(dr, textEditorControl);
                         break;
