1f45db1944b8b625a2d35b3405df2a9159375619
xeL <xcoding.dev@gmail.com>
2024-05-04 16:42:23 +0300

    created class for clear current line. col posiiton and numbring

---
+++ CIARE/GUI/ClearInfoLinescs.cs
@@ -0,0 +1,17 @@
+﻿using CIARE.Utils;
+using System.Runtime.Versioning;
+
+namespace CIARE.GUI
+{
+    public class ClearInfoLinescs
+    {
+        [SupportedOSPlatform("windows")]
+        public static void ClearLinesInfo()
+        {
+            MainForm.Instance.linesPositionLbl.Text = string.Empty;
+            MainForm.Instance.linesCountLbl.Text = string.Empty;
+            GlobalVariables.linePos = 0;
+            GlobalVariables.colPos = 0;
+        }
+    }
+}
