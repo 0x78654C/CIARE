aed1a9370bbaf37438eaf23ee02705d05cb0236b
xeL <xcoding.dev@gmail.com>
2024-05-04 22:21:41 +0300

    added assmbly load class used in final unload.

---
+++ CIARE/Utils/Options/AsmLoad.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIARE.Utils.Options
+{
+    public class AsmLoad: AssemblyLoadContext
+    {
+        private AssemblyDependencyResolver _resolver;
+
+        public AsmLoad(string mainAssemblyToLoadPath) : base(isCollectible: true)
+        {
+            _resolver = new AssemblyDependencyResolver(mainAssemblyToLoadPath);
+        }
+
+        protected override Assembly? Load(AssemblyName name)
+        {
+            string? assemblyPath = _resolver.ResolveAssemblyToPath(name);
+            if (assemblyPath != null)
+            {
+                return LoadFromAssemblyPath(assemblyPath);
+            }
+
+            return null;
+        }
+    }
+}
