85a3e3d25fc99a0af86829d02f0ab343ab8c900d
xeL <xcoding.dev@gmail.com>
2024-05-05 21:17:15 +0300

    cleanup

--- CIARE/Utils/Options/AsmLoad.cs
+++ CIARE/Utils/Options/AsmLoad.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Runtime.Loader;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CIARE.Utils.Options
 {
-    public class AsmLoad: AssemblyLoadContext
+    public class AsmLoad : AssemblyLoadContext
     {
         private AssemblyDependencyResolver _resolver;
 
