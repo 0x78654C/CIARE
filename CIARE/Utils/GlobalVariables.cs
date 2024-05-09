3d64d8cb6b4642f26943a35f0570b9312377961e
xeL <xcoding.dev@gmail.com>
2024-05-09 21:54:24 +0300

    removed unused global variable

--- CIARE/Utils/GlobalVariables.cs
+++ CIARE/Utils/GlobalVariables.cs
@@ -84,7 +84,6 @@ namespace CIARE.Utils
         public static readonly string OPlatformParam = "OPlatformParam";
         public static readonly string OFramework = "OFramework";
         public static readonly string OlastTabPosition = "lastTabPosition";
-        public static int linePos = 0;
         public static int selectedIndex = 0;
         //output richtextbox zoomfactor varlue store;
         public static float zoomFactor = 1f;
