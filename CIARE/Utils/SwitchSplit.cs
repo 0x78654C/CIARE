c756a51ee1482a5e019ff77878330b6f481b044f
xeL <xcoding.dev@gmail.com>
2024-05-21 22:11:38 +0300

    created class for switch split window in curent editor tab

---
+++ CIARE/Utils/SwitchSplit.cs
@@ -0,0 +1,30 @@
+﻿using ICSharpCode.TextEditor;
+
+namespace CIARE.Utils
+{
+    /*
+     Split ICSharpCode.TextEditor window.
+     */
+    public class SwitchSplit
+    {
+        /// <summary>
+        /// Split text editor window.
+        /// </summary>
+        /// <param name="textEditor"></param>
+        public static void SwitchSplitWindow()
+        {
+            var firstArea = GlobalVariables.textAreaFirst;
+            var secondArea = GlobalVariables.textAreaSecond;
+            if ((firstArea != null) && firstArea.TextArea.Focused)
+            {
+                if (secondArea != null)
+                    secondArea.TextArea.Focus();
+            }
+            else
+            {
+                if (firstArea != null)
+                    firstArea.TextArea.Focus();
+            }
+        }
+    }
+}
