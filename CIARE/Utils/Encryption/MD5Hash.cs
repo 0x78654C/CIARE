bb1fb90e0822fe238a81bb4c7dfe9a40ce2238e0
x_Coding <xcoding.dev@gmail.com>
2024-05-31 00:31:58 +0300

    created MD5 checker class

---
+++ CIARE/Utils/Encryption/MD5Hash.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CIARE.Utils.Encryption
+{
+    public class MD5Hash
+    {
+        /// <summary>
+        /// Get MD5 hash from string.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static string GetMD5Hash(string data)
+        {
+            string result = "";
+            if (string.IsNullOrEmpty(data)) return "";
+            byte[] bytes = Encoding.ASCII.GetBytes(data);
+            using (var md5 = MD5.Create())
+            {
+                var hash = md5.ComputeHash(bytes);
+                result = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+            }
+            return result;
+        }
+    }
+}
