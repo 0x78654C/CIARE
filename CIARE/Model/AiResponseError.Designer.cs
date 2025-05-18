7a4aeb6166111684e3251dfe8a2c072e0a918078
x_coding <xcoding.dev@gmail.com>
2025-05-19 01:18:06 +0300

    created  form for error ai respond data

---
+++ CIARE/Model/AiResponseError.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace CIARE.Model
+{
+    partial class AiResponseError
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AiResponseError));
+            textEditorControl = new ICSharpCode.TextEditor.TextEditorControl();
+            SuspendLayout();
+            // 
+            // textEditorControl
+            // 
+            textEditorControl.AllowDrop = true;
+            textEditorControl.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            textEditorControl.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            textEditorControl.Highlighting = "C#-Dark";
+            textEditorControl.Location = new System.Drawing.Point(12, 12);
+            textEditorControl.Name = "textEditorControl";
+            textEditorControl.ReadOnly = true;
+            textEditorControl.ShowLineNumbers = false;
+            textEditorControl.ShowVRuler = false;
+            textEditorControl.Size = new System.Drawing.Size(827, 760);
+            textEditorControl.TabIndex = 1;
+            // 
+            // AiResponseError
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(851, 784);
+            Controls.Add(textEditorControl);
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Name = "AiResponseError";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "AI respond on error message";
+            Load += AiResponseError_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ICSharpCode.TextEditor.TextEditorControl textEditorControl;
+    }
+}
---
+++ CIARE/Model/AiResponseError.cs
@@ -0,0 +1,28 @@
+﻿/*
+    Display AI response in a new form.
+*/
+using CIARE.GUI;
+using CIARE.Utils;
+using System;
+using System.Runtime.Versioning;
+using System.Windows.Forms;
+
+namespace CIARE.Model
+{
+    [SupportedOSPlatform("windows")]
+    public partial class AiResponseError : Form
+    {
+        public AiResponseError()
+        {
+            InitializeComponent();
+        }
+
+        private void AiResponseError_Load(object sender, EventArgs e)
+        {
+            InitializeEditor.ReadEditorHighlight(GlobalVariables.registryPath, textEditorControl, new ComboBox { });
+            FrmColorMod.ToogleColorMode(this, GlobalVariables.darkColor);
+            textEditorControl.Text = GlobalVariables.errorAiResponse;
+            GlobalVariables.errorAiResponse = "";
+        }
+    }
+}
