93ff20334d3f5279826c676f3b6f2a4b42fd517a
xeL <xcoding.dev@gmail.com>
2025-08-12 21:54:14 +0300

    added CTRL- Home / End for text selection in ai response

--- CIARE/Model/AiResponse.cs
+++ CIARE/Model/AiResponse.cs
@@ -26,6 +26,36 @@ namespace CIARE.Model
             Text = $"{GlobalVariables.aiTypeVar} response:";
             TextBoxWrap();
         }
+        
+        /// <summary>
+        /// Hot keys decalaration.
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="keyData"></param>
+        /// <returns></returns>
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.End | Keys.Control:
+                    if (!string.IsNullOrEmpty(SelectedEditor.GetSelectedEditor().Text))
+                    {
+                        var liensCount = SelectedEditor.GetSelectedEditor().Document.TotalNumberOfLines;
+                        SelectedEditor.GetSelectedEditor().ActiveTextAreaControl.TextArea.ScrollTo(liensCount);
+                        SelectedEditor.GetSelectedEditor().ActiveTextAreaControl.TextArea.Caret.Line = liensCount;
+                    }
+                    return true;
+                case Keys.Home | Keys.Control:
+                    if (!string.IsNullOrEmpty(SelectedEditor.GetSelectedEditor().Text))
+                    {
+                        SelectedEditor.GetSelectedEditor().ActiveTextAreaControl.TextArea.ScrollTo(0);
+                        SelectedEditor.GetSelectedEditor().ActiveTextAreaControl.TextArea.Caret.Line = 0;
+                    }
+                    return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
 
         private void textEditorControl_Resize(object sender, EventArgs e) => TextBoxWrap();
 
